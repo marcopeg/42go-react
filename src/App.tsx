2d0d7630ef81bfdcb86a6d55754d36acbd214f67
marcopeg <marco.pegoraro@gmail.com>
2025-04-28 10:18:56 +0200

    fix color

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { Button } from "@/components/ui/button";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
+      <h1 className="text-3xl font-bold mb-4">Welcome to My App</h1>
       <Button>Click me</Button>
+      <Button variant={"secondary"}>Click me</Button>
+      <Button variant={"outline"}>Click me</Button>
     </div>
   );
 }
