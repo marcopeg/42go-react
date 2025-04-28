620491b7153d88c78c84fcb1bdce1b6704bac4b9
marcopeg <marco.pegoraro@gmail.com>
2025-04-28 10:21:47 +0200

    button

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ function App() {
     <div className="flex flex-col items-center justify-center min-h-svh">
       <h1 className="text-3xl font-bold mb-4">Welcome to My App</h1>
       <Button>Click me</Button>
-      <Button variant={"secondary"}>Click me</Button>
-      <Button variant={"outline"}>Click me</Button>
     </div>
   );
 }
